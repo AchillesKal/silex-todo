aadec52496773cff571fb8fa6ae6f52a4adeff52
Achilles Kaloeridis <achilleas90gr@gmail.com>
2016-04-25 17:44:22 +0300

    Silex, Front-controller and a route added

---
+++ web/app.php
@@ -0,0 +1,13 @@
+<?php
+
+require_once __DIR__.'/../vendor/autoload.php';
+
+$app = new Silex\Application();
+
+$app['debug'] = true;
+
+$app->get('/', function (){
+        return 'Homepage';
+});
+
+$app->run();
