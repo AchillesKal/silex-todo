721f466865c2e91d95e45132f121d962bbc9b567
Achilles Kaloeridis <achilleas90gr@gmail.com>
2016-04-26 02:36:26 +0300

    Files seperation

---
+++ app/app.php
@@ -0,0 +1,27 @@
+<?php
+
+use Silex\Application;
+use Silex\Provider\FormServiceProvider;
+
+$app = new Application();
+
+$app['debug'] = true;
+
+$app->register(new Silex\Provider\TwigServiceProvider(), array(
+    'twig.path' => __DIR__.'/../app/views',
+));
+$app->register(new FormServiceProvider());
+$app->register(new Silex\Provider\ValidatorServiceProvider());
+$app->register(new Silex\Provider\TranslationServiceProvider(), array(
+    'translator.domains' => array(),
+));
+$app->register(new Silex\Provider\DoctrineServiceProvider(), array(
+    'db.options' => array(
+        'driver'   => 'pdo_sqlite',
+        'path'     => __DIR__.'/../app/app.db',
+    ),
+));
+$app->register(new Silex\Provider\UrlGeneratorServiceProvider());
+
+
+return $app;
---
+++ web/index.php
@@ -0,0 +1,8 @@
+<?php
+
+require_once __DIR__.'/../vendor/autoload.php';
+
+$app = require __DIR__.'/../app/app.php';
+require __DIR__.'/../app/config/routes.php';
+
+$app->run();
