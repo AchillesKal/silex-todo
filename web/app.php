887ddb4cbcdfaf74325f98106b42e450ccbbbe6b
Achilles Kaloeridis <achilleas90gr@gmail.com>
2016-04-25 20:32:18 +0300

    FormServiceProvider

--- web/app.php
+++ web/app.php
@@ -2,6 +2,12 @@
 
 require_once __DIR__.'/../vendor/autoload.php';
 
+use Silex\Provider\FormServiceProvider;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Form\Extension\Core\Type\FormType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+
 $app = new Silex\Application();
 
 $app['debug'] = true;
@@ -9,9 +15,45 @@ $app['debug'] = true;
 $app->register(new Silex\Provider\TwigServiceProvider(), array(
     'twig.path' => __DIR__.'/../app/views',
 ));
+$app->register(new FormServiceProvider());
+$app->register(new Silex\Provider\ValidatorServiceProvider());
+$app->register(new Silex\Provider\TranslationServiceProvider(), array(
+    'translator.domains' => array(),
+));
 
 $app->get('/', function () use ($app){
         return $app['twig']->render('index.html.twig');
 })->bind('homepage');
+
+$app->get('/todo/{id}', function ($id) use ($app){
+        return $id;
+})->bind('new_todo');
+
+$app->match('/form', function (Request $request) use ($app) {
+        // some default data for when the form is displayed the first time
+        $data = array(
+            'name' => 'Your name',
+            'email' => 'Your email',
+        );
+        $form = $app['form.factory']->createBuilder(FormType::class, $data)
+            ->add('name', TextType::class)
+            ->add('email', EmailType::class)
+            ->getForm();
+
+
+        $form->handleRequest($request);
+
+//        if ($form->isValid()) {
+//                $data = $form->getData();
+//
+//                // do something with the data
+//
+//                // redirect somewhere
+//                return 'vrel';
+//        }
+
+        // display the form
+        return $app['twig']->render('index.html.twig', array('form' => $form->createView()));
+});
 
 $app->run();
