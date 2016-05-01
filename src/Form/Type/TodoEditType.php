42eecbd873f961d73123bcf90b51282c7680eaf8
Achilles Kaloeridis <achilleas90gr@gmail.com>
2016-05-02 00:22:28 +0300

    textarea field in edit form

--- src/Form/Type/TodoEditType.php
+++ src/Form/Type/TodoEditType.php
@@ -3,6 +3,7 @@
 namespace Todo\Form\Type;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
@@ -16,7 +17,7 @@ class TodoEditType extends AbstractType
     {
         $builder
             ->add('name', TextType::class)
-            ->add('description', TextType::class)
+            ->add('description', TextareaType::class)
             ->add('is_done', CheckboxType::class, array(
                 'required'    => false,
             ))
